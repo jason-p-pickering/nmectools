f8b200061c13bbf9ffc771d9c70c46273d5f241f
Jason Pickering <jason.p.pickering@gmail.com>
2022-04-18 14:52:51 +0200

    Updates

--- R/sendWeeklyReportByEmail.R
+++ R/sendWeeklyReportByEmail.R
@@ -1,7 +1,16 @@
+#' Title
+#'
+#' @param d Data object
+#' @param d2_session datimutils session
+#'
+#' @return
+#' @export
+#'
+
 sendWeeklyReportByEmail<-function(d,d2_session) {
   #Format the mail
   from<-"noreply@dhis.co.zm"
-  subject<-paste("Weekly Topup Report for ", as.Date(d$report_date)-days(7) , "-",d$report_date)
+  subject<-paste("Weekly Topup Report for ", as.Date(d$report_date)- lubridate::days(7) , "-",d$report_date)
 
   msg <- sendmailR::mime_part(paste('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0
 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -18,7 +27,7 @@ Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 </html>'))
 
   msg[["headers"]][["Content-Type"]] <- "text/html"
-  to<-getReportRecipients(d2_default_session)
+  to<-getReportRecipients(d2_session)
   body<-list(body_text = msg, sendmailR::mime_part(d$zip_file_name))
   sendmailR::sendmail(from,to,subject,body)
 }
